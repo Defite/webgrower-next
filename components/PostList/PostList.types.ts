b91a3755e5aeb001996ebf2eee5cf2f5e3406429
Defite <me@defite.ru>
2022-05-18 10:39:31 +0300

    fix(vercel): vercel build errors

--- components/PostList/PostList.types.ts
+++ components/PostList/PostList.types.ts
@@ -1,9 +1,10 @@
 import {PostProps} from "../Post/Post.types";
+import {CategoryColors} from '../CategorySelect/CategorySelect.types';
 
 export type PostListItem = {
     content: PostProps;
     created_at: Date;
-    tag_list: Array<string>;
+    tag_list: Array<keyof typeof CategoryColors>;
     slug: string;
 };
 
